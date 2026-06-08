d896e2cfdffcda6bb640ccc1082522807f0a41ba
Thanhle1067 <51292501+Thanhle1067@users.noreply.github.com>
2026-06-08 21:17:59 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-exec /usr/local/bin/xmrig \
-  -a "${ALGO:-rx/0}" \
-  -o "${POOL}" \
-  -u "${WALLET}" \
-  -p "${PASS:-x}" \
-  --rig-id "${WORKER:-akash}" \
-  ${TLS:+--tls}
+ARGS="
+-a ${ALGO:-rx/0}
+-o ${POOL}
+-u ${WALLET}
+-p ${PASS:-x}
+--rig-id ${WORKER:-akash}
+"
+
+if [ "${TLS}" = "true" ]; then
+  ARGS="$ARGS --tls"
+fi
+
+exec /usr/local/bin/xmrig $ARGS
